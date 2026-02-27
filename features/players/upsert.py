2103e02d21d26d29673a6d43b55bde03456c41d5
git-cameronbryce <developmentcameronbryce@gmail.com>
2026-02-27 09:47:17 -0700

    changed last_online to unix

--- features/players/upsert.py
+++ features/players/upsert.py
@@ -1,4 +1,5 @@
 from utils.redis import client
+from datetime import datetime
 import json
 
 async def upsert(results, guild_id, server_id):
@@ -10,11 +11,13 @@ async def upsert(results, guild_id, server_id):
     if not result["online"]:
       continue
 
+    last_online = int(datetime.fromisoformat(result["last_online"]).timestamp())
+    
     players.append({
       "id": result["id"],
       "gamertag": result["name"],
       "is_online": result["online"],
-      "last_online": result["last_online"]
+      "last_online": last_online
     })
 
   key = f"guild:{guild_id}:server:{server_id}:players"
